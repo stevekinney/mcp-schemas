04c78c413d985e8483638b63691f794d2a86c830
Steve Kinney <hello@stevekinney.net>
2025-05-16 13:08:50 -0600

    Task 1: Abstract Schema Extensions with Helpers

--- src/schemas/ping-request.ts
+++ src/schemas/ping-request.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 import type { PingRequest } from "../schema";
 import { ZodType } from "zod";
-import { requestSchema } from "./request";
+import { withRequest } from "./utils";
 
 /**
  * A ping, issued by either the server or the client, to check that the other party is still alive. The receiver must promptly respond, or else may be disconnected.
  */
-export const pingRequestSchema = requestSchema.extend({
-  method: z.literal("ping"),
-}) satisfies ZodType<PingRequest>;
+export const pingRequestSchema = withRequest("ping", {}) satisfies ZodType<PingRequest>;
---
+++ src/schemas/utils.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { requestSchema } from './request';
+import { notificationSchema } from './notification';
+
+export function withRequest<T extends z.ZodRawShape>(
+  method: string,
+  params: T,
+) {
+  return requestSchema.extend({
+    method: z.literal(method),
+    params: z.object(params),
+  });
+}
+
+export function withNotification<T extends z.ZodRawShape>(
+  method: string,
+  params: T,
+) {
+  return notificationSchema.extend({
+    method: z.literal(method),
+    params: z.object(params),
+  });
+}
